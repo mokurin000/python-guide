9e8440a5bdd3b4f28b321c886e13c5759aef4d7e
莯凛 <mokurin000@gmail.com>
2026-06-18 20:46:19 +0800

    test: fix answer of triangle-validation

--- exercise/09-a/answer/01-triangle-validation.py
+++ exercise/09-a/answer/01-triangle-validation.py
@@ -6,12 +6,9 @@ c = int(input())
 # 如果可以构成，则输出: o
 # 如果无法构成，则输出: x
 
-# 删掉此行后补充实现。
-raise NotImplementedError("代码未实现")
-
 # 此处 `...` 表示代码中被省略掉的部分，你应该把它换成真正的逻辑部分。
 # 提示: 三角形成立条件：任意两边长之和大于第三边长。
-if ...:
+if a + b > c and a + c > b and b + c > a:
     triangle_validation = "o"
 else:
     triangle_validation = "x"
