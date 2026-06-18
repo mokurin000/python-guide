51e47f377294bb7a45e75aa557cbfb02ca04d25d
莯凛 <mokurin000@gmail.com>
2026-06-18 20:17:45 +0800

    docs: add conditional expression

---
+++ exercise/09-a/01-triangle-validation.py
@@ -0,0 +1,17 @@
+# 输入三条边长，验证它们是否可以构成三角形。
+a = int(input())
+b = int(input())
+c = int(input())
+
+# 如果可以构成，则输出: o
+# 如果无法构成，则输出: x
+
+# 删掉此行后补充实现。
+raise NotImplementedError("代码未实现")
+
+# 此处 `...` 表示代码中被省略掉的部分，你应该把它换成真正的逻辑部分。
+# 提示: 三角形成立条件：任意两边长之和大于第三边长。
+if ...:
+    triangle_validation = "o"
+else:
+    triangle_validation = "x"
