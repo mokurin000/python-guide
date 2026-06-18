5851ec437912cdb4abed06eed2ba733370ce43b7
莯凛 <mokurin000@gmail.com>
2026-06-18 09:08:03 +0800

    todo: chapter 08, basic builtins

--- exercise/06/02-rectangle.py
+++ exercise/06/02-rectangle.py
@@ -3,7 +3,7 @@
 宽 = int(input())
 
 # 实现后删除此行。
-raise NotImplementedError()
+raise NotImplementedError("代码未实现")
 
 # 从这里开始编写，
 # 输出拥有以上长宽的矩形面积。
---
+++ exercise/08/01-bmi.py
@@ -0,0 +1,7 @@
+# 输入体重 (kg)
+weight = float(input())
+# 输入身高 (cm)
+height = int(input()) / 100
+
+# 目标：输出四舍五入，保留小数点后两位的计算结果
+raise NotImplementedError("代码未实现")
